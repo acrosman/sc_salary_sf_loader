31bad1fdd64d80c89996665772e15ba893e5efae
Aaron Crosman <acrosman@gmail.com>
2025-03-15 20:49:40 -0700

    Adding config script

---
+++ config.py
@@ -0,0 +1,7 @@
+DEFAULT_CONFIG = {
+    'batch_size': 10000,
+    'max_retries': 3,
+    'retry_delay': 5,
+    'log_level': 'INFO',
+    'log_format': '%(asctime)s - %(levelname)s - %(message)s'
+}
